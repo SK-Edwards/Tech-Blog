c44d667cf581339467b82bcc980f34a840595c79
SK. Edwards <125165568+SK-Edwards@users.noreply.github.com>
2023-05-15 01:28:39 -0400

    Made database and connected server

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS the_long_walk_db;
+
+CREATE DATABASE the_long_walk_db;
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const routes = require('./routes')
+const controllers = require('./controllers')
+const sequelize = require('./config/connection');
 const PORT = 3001
 const app = express();
 
-app.use(routes);
+app.use(controllers);
 
 
-app.listen(PORT, () => {
-    console.log('LETS GET TO WORK');
+
+
+
+sequelize.sync({}).then(() => {
+     app.listen(PORT, () => {
+       console.log('LETS GET TO WORK');
+  })
 })
